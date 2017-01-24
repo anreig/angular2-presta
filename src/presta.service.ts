161dc5a068d2b8f0322905f12b8033d63f031ab3
SKaDiZZ <akirapowered@gmail.com>
2017-01-24 18:40:54 +0100

    Updated documentation ...

--- src/presta.service.ts
+++ src/presta.service.ts
@@ -1,6 +1,6 @@
-import {Injectable, Inject} from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { PrestaConfiguration, Query } from './presta.interfaces';
