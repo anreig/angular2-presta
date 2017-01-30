217ed608a7915fcf6facf8047adfa544a7373f03
SKaDiZZ <akirapowered@gmail.com>
2017-01-30 13:55:24 +0100

    added new component exports

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import {NgModule, ModuleWithProviders} from "@angular/core";
-import {CommonModule} from "@angular/common";
+import {NgModule, ModuleWithProviders} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import {PrestaPipe} from "./src/presta.pipe";
-import {PrestaService} from "./src/presta.service";
-import {PrestaConfiguration} from "./src/presta.interfaces";
+import {PrestaPipe} from './src/presta.pipe';
+import {PrestaService} from './src/presta.service';
+import {PrestaConfiguration} from './src/presta.interfaces';
+import {PrestaImage} from './src/presta.components';
 
 export * from './src/presta.pipe';
 export * from './src/presta.service';
 export * from './src/presta.interfaces';
+export * from './src/presta.components';
 
 @NgModule({
   imports: [
